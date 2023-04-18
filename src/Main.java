030be8fae10137e58063d2a379c9b51c2ae37598
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-04-18 15:11:24 +0530

    function.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.print(sum());
+    }
+
+static int sum(){
+    int a,b,c;
+    Scanner i = new Scanner(System.in);
+    System.out.println("Enter First NUmber");
+    a = i.nextInt();
+    b = i.nextInt();
+    return c = a + b;
+}
+}
