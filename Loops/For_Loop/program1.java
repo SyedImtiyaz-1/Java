268e0c2a495003fd52022ba4c9c64055512ab7bf
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-04-18 17:26:44 +0530

    for-loop

---
+++ Loops/For_Loop/program1.java
@@ -0,0 +1,8 @@
+//print 5 numbers
+public class program1{
+    public static void main(String[]args){
+        for(int i=1; i<=5;i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ Loops/For_Loop/program2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class program2 {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        System.out.print("Enter any number : ");
+        int a = x.nextInt();
+        for (int i = 1; i <= a ; i++) {
+            System.out.println(i);
+        }
+    }
+}
