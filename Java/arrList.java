57add2e7c91e8476aac2359de9fa8f5784becfb9
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-05-02 13:27:23 +0530

    ArrayList

---
+++ Java/arrList.java
@@ -0,0 +1,32 @@
+//import java.util.Scanner;
+//import java.util.ArrayList;
+
+//public class arrList {
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner (System.in);
+//        ArrayList<Integer> list = new ArrayList<>(10);
+
+//        list.add(50);
+//        list.add(50);
+//        list.add(50);
+//        list.add(50);
+
+//        list.remove(2);
+//        list.set(0,100);
+//        System.out.println(list.contains(10));
+//        list.add(20);
+//        System.out.println(list);
+//
+//        for(int i = 0; i < 5; i++){
+//            list.add(in.nextInt());
+//        }
+
+//        System.out.println(list);
+
+//        for(int i = 0; i < 5; i++){
+//            System.out.println(list.get(i));
+//        }
+
+//    }
+
+//}
